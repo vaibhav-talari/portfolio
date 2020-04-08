f0dc78cc1c02d48b1c4744ee7821e2a7e3d675e9
Vaibhav <vaibhavtalari15@gmail.com>
2020-04-08 21:45:41 +0530

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="app">
+    <Header
+		:nav-links="navLinks"
+		:image-path="require('./assets/logo.png')"
+		background="#fff"
+		link-color="#777"
+		hoverBackground="#ddd"
+    />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import Header from '~/components/Header.vue'
+import Footer from '~/components/Footer.vue'
+
+
+export default {
+  components: {
+    Logo,
+    Header,
+    Footer
+  },
+  data: () => ({
+    navLinks: [
+      {
+        text: 'Contact',
+		path: '/contact',
+		icon: 'ion-ios-megaphone'
+      },
+      {
+        text: 'About',
+		path: '/about',
+		icon: 'ion-ios-business'
+      },
+      {
+        text: 'Blog',
+		path: '/blog',
+		icon: 'ion-ios-bonfire'
+      },
+      {
+        text: 'Portfolio',
+		path: '/portfolio',
+		icon: 'ion-ios-briefcase'
+      }
+    ]
+  })
+}
+</script>
+
+
+
+<style lang="scss" scoped>
+@import 'https://unpkg.com/ionicons@4.2.2/dist/css/ionicons.min.css';
+
+figure {
+	margin-block-start: 0;
+	margin-block-end: 0;
+	margin-inline-start: 10px;
+	margin-inline-end: 0;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
